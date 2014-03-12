4cd8c9a11b1b88655123e157b39dd96fef8f6369
Huiming Teo <teohuiming@gmail.com>
2014-03-12 11:32:50 +0800

    added before_import/after_import callbacks

--- lib/active_admin_importable/dsl.rb
+++ lib/active_admin_importable/dsl.rb
@@ -1,6 +1,6 @@
 module ActiveAdminImportable
   module DSL
-    def active_admin_importable(&block)
+    def active_admin_importable(options={}, &block)
       action_item :only => :index do
         link_to "Import #{active_admin_config.resource_name.to_s.pluralize}", :action => 'upload_csv'
       end
@@ -10,7 +10,9 @@ module ActiveAdminImportable
       end
 
       collection_action :import_csv, :method => :post do
+        options.fetch(:before_import, proc{}).call
         CsvDb.convert_save(active_admin_config.resource_class, params[:dump][:file], &block)
+        options.fetch(:after_import, proc{}).call
         redirect_to :action => :index, :notice => "#{active_admin_config.resource_name.to_s} imported successfully!"
       end
     end
